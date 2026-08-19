1b92979b983092cd123af14a6502eaf3a2ee6b82
BarinderSingh30 <emailforbarinder@gmail.com>
2026-08-19 17:53:16 +0530

    plugin: harpoon and whichkey

---
+++ lua/barinder/plugins/whichkey.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    spec = {
+      { "<leader>f", group = "Find" },
+      { "<leader>1", hidden = true },
+      { "<leader>2", hidden = true },
+      { "<leader>3", hidden = true },
+      { "<leader>4", hidden = true },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer keymaps",
+    },
+  },
+}
