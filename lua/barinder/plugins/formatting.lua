bab39e6a50980daf6d9aeeb292dff4a8e7d5e33e
BarinderSingh30 <emailforbarinder@gmail.com>
2026-08-19 17:43:47 +0530

    plugin: formatting plugin

---
+++ lua/barinder/plugins/formatting.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "ruff_format" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier", stop_after_first = true },
+      go = { "goimports", "gofmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
